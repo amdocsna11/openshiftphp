3245a886878803a85fff25353aa8a67430e7c6e0
amdocsna11 <109971803+amdocsna11@users.noreply.github.com>
2022-07-25 11:38:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 2022  Test</h1>
-		<p><b>Amdocs NA11 Session with auto deployment of changes</b></p>
+		<p><b>Amdocs NA11 Session with auto deployment of changes ...</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information </h2> 
